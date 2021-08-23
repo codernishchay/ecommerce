8e6fac0311213de9415d32676caf470fd89c8f6e
codernishchay <nishchay.vns2000@gmaiil.com>
2021-08-23 13:56:54 -0400

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,33 +5,25 @@ function Header ({size, title}) {
      
    <div class="container bg-blue-900 h-10  p-5 m-auto">
    <div class= "flex flex-row justify-between ">
-     <div class=" bg-yellow-900  w-10 h-10  rounded-lg ">
+     <div class=" bg-yellow-900  w-10 h-10 rounded-lg p-2 ">
        <h1 class="text-3xl text-red-900 ml-10">LOGO</h1>
      </div>
-     <div>
-     <a class="text-green-900 hover:text-blue-200 px-4">Home</a>
-     <a class="text-green-900 hover:text-blue-200 px-4">Home</a>
-     <a class="text-green-900 hover:text-blue-200 px-4">Home</a>
-     <a class="bg-black text-green-100 inline-block hover:text-blue-200 px-5  py-3 rounded-3xl "><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+     <div class="px-2
+     ">
+     <a  href="#" class="text-green-900 hover:text-blue-200 p-2 ">Home</a>
+     <a  href="#" class="text-green-900 hover:text-blue-200 p-2">Home</a>
+     <a  href="#" class="text-green-900 hover:text-blue-200 p-2">Home</a>
+     <a class="bg-black text-green-100 inline-block hover:text-blue-200 px-5  py-3 rounded-3xl "><svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 inline-block "  fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-</svg>Cart (0)</a>    
+</svg>Cart</a>    
      </div>
      </div>
    </div>
- 
-  <div>
-    <h2>
-      Some Catchy title here
-    </h2>
-    <p>
-      Some Catchy title here
-    </p>
-  </div>
-
    </div>
 
     ); 
 };
+
 
 export default Header;
 
--- src/components/StoreFrontBody.jsx
+++ src/components/StoreFrontBody.jsx
@@ -2,22 +2,11 @@ import Button from "./Button";
 
 function StoreFrontBody(){
     return (
-        <div class ="flex flex-row mt-20">
-
-            <div class="w-2/5  flex-col justify-center">
-            
-              <h2 class="font-serif text-3xl mb-5 ">Welcome You stupid</h2>
-              <p>Hello there how are you</p>
-              <p>Hello there how are you</p>
-              <p>Hello there how are you</p>
-              <p>Hello there how are you</p>
-              <p class="mb-5">Hello there how are you</p>
-            
-              <Button size="10" children="Hello there ! " class="my-10"></Button>
-              
-            </div>
-            
-        </div>
+         <div>
+             <div className="container">
+                
+             </div>
+         </div>
     )
 }
 
