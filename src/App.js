24d896ea61e5ed5d62991dcdec66bf6f26e742bd
codernishchay <nishchay.vns2000@gmaiil.com>
2021-08-24 05:34:28 -0400

    home page almost completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from './components/Button';
 import Header from './components/Header';
 import StoreFrontBody from './components/StoreFrontBody';
-import Card from './components/card';
-import ProductGrid from './components/productgrid';
+import ProductGrid from './components/productgrid'; 
 function App() {
   return (
     <div>
    <Header/>
    <StoreFrontBody/>
-  <ProductGrid/>
+
+   <ProductGrid/>
+   <StoreFrontBody/>
+   <ProductGrid/>
     </div>
     
   );
--- src/components/StoreFrontBody.jsx
+++ src/components/StoreFrontBody.jsx
@@ -2,9 +2,25 @@ import Button from "./Button";
 
 function StoreFrontBody(){
     return (
-         <div className="flex flex-row h-30 bg-white-900 mt-16">
+         <div className="flex flex-row bg-white-900 mt-16 ml-10">
+             <div  className= "w-1/3 flex flex-col justify-around">
+                <div className="p-10">
+                    <h1 className="text-5xl text-left">
+                        Hello Welcome to Store
+                    </h1>
+                    
+              <div>
+                   <p>
+                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint culpa reprehenderit voluptatem fugiat! Modi sequi rerum assumenda odio sit architecto t    </p>
+              </div>
+              <div className="text-center"><Button size='100'>Shop Now</Button></div>
+       
+
+          </div>
+          </div>
           <div>
-              Helllo there
+              
+             <img src="./images/undraw_Outdoor_adventure_re_j3b7.svg" className=""></img>
           </div>
          </div>
     )
