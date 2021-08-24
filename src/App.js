06d561d0f70b2f2a218bbce895b050c3e5159726
codernishchay <nishchay.vns2000@gmaiil.com>
2021-08-24 03:03:42 -0400

    .

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Button from './components/Button';
 import Header from './components/Header';
 import StoreFrontBody from './components/StoreFrontBody';
+import Card from './components/card';
 function App() {
   return (
     <div className="App">
    <Header/>
-      
-   <StoreFrontBody/>
-
+   <Card/>
     </div>
     
   );
